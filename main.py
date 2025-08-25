8cd0549929d787a7a9863150c22f61fb44743da6
44wrw44 <44_wrw_44@mail.ru>
2025-08-25 21:46:42 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
+import json
+from typing import Annotated
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import os
+from pydantic import BaseModel
 
 port: int = int(os.getenv('SOVANET_PORT', "8080"))
 hello: str = os.getenv('HELLO', "WORLD")
 
+if not os.path.exists('data'):
+    os.mkdir('data')
+
 app = FastAPI()
+
+
+class User(BaseModel):
+    name: str = "Piter"
+    age: int = 30
+    comment: str = "Nigger"
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello "+hello}
+
+
+@app.post("/user/")
+async def create_user(
+        user: User,
+):
+    with open(f"data/{user.name}.txt", "w") as file:
+        file.write(user.model_dump())
+    return {"success": True}
+
+
+@app.get("/user/{name}")
+async def get_user(
+        name: Annotated[str, Path()]
+):
+    try:
+        with open(f"data/{name}.txt", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        raise HTTPException(status_code=404)
 
 
 if __name__ == '__main__':
